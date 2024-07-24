83fc0aeebbd537e97733d021cf912bf15c78fa25
Mike Lasby <mklasby@gmail.com>
2024-07-24 16:24:35 -0600

    refactor of subdir layout, adding python package, rough implementation of ffi_linear and dense-to-sparse test

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(
     name='mlp_hip',
     ext_modules=[
-        CUDAExtension('mlp_hip', ['mlp.hip']),
+        CUDAExtension('mlp_hip', ['rocm_torch_extension/mlp.hip']),
     ],
     cmdclass={
         'build_ext': BuildExtension
-    })
+    },
+    packages=find_packages()
+    )
