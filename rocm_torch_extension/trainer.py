60fe688940bad6d8e3caa7454187dfa73376343f
mohamedyassin1 <moyassin2001@gmail.com>
2024-07-26 00:29:53 +0000

    Fix backwards pass and add streams

--- rocm_torch_extension/trainer.py
+++ rocm_torch_extension/trainer.py
@@ -19,53 +19,38 @@ print('Using device:', device)
 
 # Test #1 
 
-# batch_size = 36
-# seq_len = 12
-# feature_size = 256
-# output_size = 512
-# fan_in = 24
+batch_size = 36
+seq_len = 12
+feature_size = 256
+output_size = 512
+fan_in = 24
 
-# features = torch.randn(batch_size, seq_len, feature_size, dtype=torch.float32, requires_grad=True, device=device)
-# weights = torch.randn(output_size, fan_in, dtype=torch.float32, requires_grad=True, device=device)
-# locations = torch.randint(0, feature_size, (output_size, fan_in), dtype=torch.int32, requires_grad=False,
-#                             device=device)
+features = torch.randn(batch_size, seq_len, feature_size, dtype=torch.float32, requires_grad=True, device=device)
+weights = torch.randn(output_size, fan_in, dtype=torch.float32, requires_grad=True, device=device)
+locations = torch.randint(0, feature_size, (output_size, fan_in), dtype=torch.int32, requires_grad=False,
+                            device=device)
 
-# transpose = False
+transpose = False
 
-# # do the multiplication jointly
-# all_results = FFI.ffi_mul(features, weights, locations, None, transpose)
-# assert all_results.shape == (batch_size, seq_len, output_size)
-# torch.set_printoptions(threshold=100_000)
-# # print(all_results)
+# do the multiplication jointly
+all_results = FFI.ffi_mul(features, weights, locations, None, transpose)
+assert all_results.shape == (batch_size, seq_len, output_size)
+torch.set_printoptions(threshold=100_000)
+# print(all_results)
 
-# # and compare result with slices
-# one_result = FFI.ffi_mul(features[:, 5, :], weights, locations, None, transpose)
-# assert (all_results[:, 5, :] == one_result).all()
+# and compare result with slices
+one_result = FFI.ffi_mul(features[:, 5, :], weights, locations, None, transpose)
+assert (all_results[:, 5, :] == one_result).all()
 
-# one_result = FFI.ffi_mul(features[8, :, :], weights, locations, None, transpose)
-# assert (all_results[8, :, :] == one_result).all()
+one_result = FFI.ffi_mul(features[8, :, :], weights, locations, None, transpose)
+assert (all_results[8, :, :] == one_result).all()
 
-# # Test int16 and int32 indicies give the same result
-# locations = torch.randint(0, feature_size, (output_size, fan_in), dtype=torch.int16, requires_grad=False,
-#                             device=device)
-# int16_result = FFI.ffi_mul(features, weights, locations.to(torch.int16), None, transpose)
-# int32_result = FFI.ffi_mul(features, weights, locations.to(torch.int32), None, transpose)
-# assert (int16_result == int32_result).all()
-
-
-# batch_size = 36
-# seq_len = 12
-# feature_size = 256
-# output_size = 512
-# fan_in = 24
-
-# features = torch.randn(batch_size, seq_len, feature_size, dtype=torch.float32, requires_grad=True, device=device)
-# weights = torch.randn(output_size, fan_in, dtype=torch.float32, requires_grad=True, device=device)
-# locations = torch.randint(0, feature_size, (output_size, fan_in), dtype=torch.int32, requires_grad=False,
-#                             device=device)
-# out_grad = torch.randn(batch_size, output_size, dtype=torch.float32, requires_grad=True, device=device)
-# mlp_hip.ffi_backward_tp(features, weights, locations, out_grad)
-
+# Test int16 and int32 indicies give the same result
+locations = torch.randint(0, feature_size, (output_size, fan_in), dtype=torch.int16, requires_grad=False,
+                            device=device)
+int16_result = FFI.ffi_mul(features, weights, locations.to(torch.int16), None, transpose)
+int32_result = FFI.ffi_mul(features, weights, locations.to(torch.int32), None, transpose)
+assert (int16_result == int32_result).all()
 
 batch_size = 36
 seq_len = 12
