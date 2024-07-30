348ad5d7b83ebdae7a91da8e3fae5d36eda72448
Mike Lasby <mklasby@gmail.com>
2024-07-30 14:07:46 -0600

    some progress on bindings

--- squash/ffi_linear.py
+++ squash/ffi_linear.py
@@ -45,7 +45,7 @@ class FFILinear(nn.Module):
             self.condensed_weight = nn.Parameter(
                 weight,
                 requires_grad=False,
-            )
+            ).contigious()
 
             if hasattr(module, "bias") and module.bias is not None:
                 self.bias = nn.Parameter(
@@ -56,7 +56,7 @@ class FFILinear(nn.Module):
                 )
             else:
                 self.register_parameter("bias", None)
-            self.register_buffer("input_mask", input_mask)
+            self.register_buffer("input_mask", input_mask.contigious())
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         return FFI.ffi_mul(
